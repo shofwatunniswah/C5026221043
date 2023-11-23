5edc9da69ad217146e5b73a9a1030ff264950bed
Shof Watun Niswah <shofwatunniswah14@gmail.com>
2023-11-23 14:55:21 +0700

    Pertemuan tgl 23 Nop 2023

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,10 @@ Route::get('/blog/tentang', function () {
 Route::get('/blog/kontak', function () {
     return view('kontak');
 });
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
